ea139d4f8ce4602bebb7fe723d9a3aae23cea18a
Nathan <drugen@gmail.com>
2022-02-19 00:00:01 -0700

    initial

--- goblins/core/game.py
+++ goblins/core/game.py
@@ -7,39 +7,42 @@ from goblins.world import World
 
 class Game:
     def __init__(self):
-        self._running = True
-        self._clock = None
-        self._display_surf = None
+        self.running = True
+        self.clock = None
+        self.display_surf = None
         self.size = self.width, self.height = WINDOW_WIDTH, WINDOW_HEIGHT
         self.world = World()
 
     def on_init(self):
         try:
             pygame.init()
-            self._clock = pygame.time.Clock()
-            self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+            self.clock = pygame.time.Clock()
+            self.display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
             return True
         except:
             return False
 
     def on_event(self, event):
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                self.running = False
         if event.type == pygame.QUIT:
-            self._running = False
+            self.running = False
 
     def on_loop(self):
-        self._clock.tick(30)
+        self.clock.tick(30)
 
     def on_render(self):
-        self.world.layer_list[0].draw(self._display_surf)
+        self.world.layer_list[0].draw(self.display_surf)
         pygame.display.flip()
 
     def on_cleanup(self):
         pygame.quit()
 
     def on_execute(self):
-        self._running = self.on_init()
+        self.running = self.on_init()
 
-        while self._running:
+        while self.running:
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
